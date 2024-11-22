af768dc48cb3c49ef64740a80c884df89a665084
Gustavo Dominguez <gfdominguez119@gmail.com>
2024-11-22 16:33:35 -0300

    Commit inicial

---
+++ frontend/src/views/pages/AboutPage.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<div class="text-6xl">About</div>
+</template>
---
+++ frontend/src/views/pages/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+<template>
+	<main>
+		<div class="text-6xl">Hello World</div>
+		<RouterLink to="/about">
+			<Button>Click Me</Button>
+		</RouterLink>
+	</main>
+</template>
